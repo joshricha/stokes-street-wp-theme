c0b30b672407ff4408ed9e067181b1725a5ff93b
Josh Richardson <joshrichardson1234@hotmail.com>
2015-11-13 17:04:16 +1100

    Work finished

--- single-work.php
+++ single-work.php
@@ -1,1 +1,54 @@
-<h1>work works</h1>
+<?php get_header(); ?>
+
+<!-- Full Page Image Background Carousel Header -->
+<section id="myCarousel" class="carousel slide">
+
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+        <li data-target="#myCarousel" data-slide-to="4"></li>
+    </ol>
+    <!-- Wrapper for Slides -->
+    <div class="carousel-inner">
+        <div class="item active">
+            <!-- Set the first background image using inline CSS below. -->
+            <div class="fill" style="background-image:url(<?php echo $carousel_image_1['url']; ?>); background-size:cover;"></div>
+            <div class="carousel-caption">
+                <h2><?php echo $carousel_image_1['title']; ?></h2>
+            </div>
+        </div>
+        <div class="item">
+            <!-- Set the second background image using inline CSS below. -->
+            <div class="fill" style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/assets/img/bg.jpg);"></div>
+            <div class="carousel-caption">
+                <h2>Caption 2</h2>
+            </div>
+        </div>
+        <div class="item">
+            <!-- Set the third background image using inline CSS below. -->
+            <div class="fill" style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/assets/img/bg.jpg);"></div>
+            <div class="carousel-caption">
+                <h2>Caption 3</h2>
+            </div>
+        </div>
+        <div class="item">
+            <!-- Set the third background image using inline CSS below. -->
+            <div class="fill" style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/assets/img/bg.jpg);"></div>
+            <div class="carousel-caption">
+                <h2>Caption 4</h2>
+            </div>
+        </div>
+        <div class="item">
+            <!-- Set the third background image using inline CSS below. -->
+            <div class="fill" style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/assets/img/bg.jpg);"></div>
+            <div class="carousel-caption">
+                <h2>Caption 5</h2>
+            </div>
+        </div>
+    </div>
+</section><!-- Carousel -->
+
+<?php get_footer(); ?>
--- template-parts/content-work.php
+++ template-parts/content-work.php
@@ -1,136 +1,29 @@
-<?php  
-
-// Custom Posts
-$subtitle       = get_field('subtitle');
-
-?>
-
 <div class="row">
-<?php $loop = new WP_Query( array('post_type' => 'work', 'orderby' => 'post_id', 'order' => 'ASC') ); ?>
+    
+    <?php $loop = new WP_Query( array('post_type' => 'work', 'orderby' => 'post_id', 'order' => 'ASC') ); ?>
 
-<?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+    <?php while( $loop->have_posts() ) : $loop->the_post(); ?>
 
-<div class="col-md-4 col-sm-4">
-    <div class="work-blocks">
-        <a href="<?php echo get_post_permalink(); ?>" class="overlay-wrapper" target="_blank">
-            <div class="overlay-text">
-                <h3><?php the_title(); ?><br>
-                <span class="work-subtitle"><?php echo $subtitle[0]; ?></span></h3>
-            </div>
-            <img class="img-hover" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/work/mfg.jpg" alt="MFG">
-        </a>
-    </div><!-- work blocks -->     
-</div><!-- col-md-4 -->
+        <?php 
 
-<?php endwhile; wp_reset_query(); ?>
+            // Custom Posts
+            $subtitle       = get_field('subtitle');
+            $hero_image     = get_field('image_1');
 
+        ?>
 
+        <div class="col-md-4 col-sm-4">
+            <div class="work-blocks">
+                <a href="<?php echo get_post_permalink(); ?>" class="overlay-wrapper" target="_blank">
+                    <div class="overlay-text">
+                        <h3><?php the_title(); ?><br>
+                        <span class="work-subtitle"><?php echo $subtitle; ?></span></h3>
+                    </div>
+                    <img class="img-hover" src="<?php echo $hero_image; ?>" alt="MFG">
+                </a>
+            </div><!-- work blocks -->     
+        </div><!-- col-md-4 -->
 
+    <?php endwhile; wp_reset_query(); ?>
 
-
-<div class="col-md-4 col-sm-4">
-    <div class="work-blocks">
-        <a href="case-study.html" class="overlay-wrapper">
-            <div class="overlay-text">
-                <h3>GETTING A SCHOOLING<br>
-                <span class="work-subtitle">MATHEW FLIDERS GIRL COLLEGE REBRANDING</span></h3>
-            </div>
-            <img class="img-hover" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/work/mfg.jpg" alt="MFG">
-        </a>
-    </div><!-- work blocks -->     
-</div><!-- col-md-4 -->
-<div class="col-md-4 col-sm-4">
-    <div class="work-blocks">
-        <a href="#" class="overlay-wrapper">
-            <div class="overlay-text">
-                <h3>RETHINKING FOOD & WINE AWARDS<br>
-                <span class="work-subtitle">RASV EPICURE AWARDS BRANDING SUITE</span></h3>
-            </div>
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/work/distilled-spirits.jpg" alt="Distilled Spirits">
-        </a>
-    </div><!-- work blocks -->   
-</div><!-- col-md-4 -->
-<div class="col-md-4 col-sm-4">
-    <div class="work-blocks">
-        <a href="#" class="overlay-wrapper">
-            <div class="overlay-text">
-                <h3>CREATING HISTORY FROM SCRATCH<br>
-                <span class="work-subtitle">MANDALA WINES BRAND CREATION</span></h3>
-            </div>
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/work/mandala.jpg" alt="Mandala">
-        </a>
-    </div><!-- work blocks -->   
-</div><!-- col-md-4 -->
-</div><!-- row -->
-<div class="row">
-<div class="col-md-4 col-sm-4">
-    <div class="work-blocks">
-        <a href="#" class="overlay-wrapper">
-            <div class="overlay-text">
-                <h3>GVING A BOOK FOR INSTITUTION<br>
-                <span class="work-subtitle">ST KILDA FESTIVAL FUNDRAISER CAMPAIGN</span></h3>
-            </div>
-            <img class="img-hover" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/work/st-kilda-festival.jpg" alt="St Kilda Festival">
-        </a>
-    </div><!-- work blocks -->     
-</div><!-- col-md-4 -->
-<div class="col-md-4 col-sm-4">
-    <div class="work-blocks">
-        <a href="#" class="overlay-wrapper">
-            <div class="overlay-text">
-                <h3>STRIPPING BACK THE SUPERFLUOUS<br>
-                <span class="work-subtitle">HOMEGROUND ANNUAL REPORT</span></h3>
-            </div>
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/work/homeground-annual-report.jpg" alt="Distilled Spirits">
-        </a>
-    </div><!-- work blocks -->   
-</div><!-- col-md-4 -->
-<div class="col-md-4 col-sm-4">
-    <div class="work-blocks">
-        <a href="#" class="overlay-wrapper">
-            <div class="overlay-text">
-                <h3>SHOWING REAL RESULTS<br>
-                <span class="work-subtitle">MATHEW FLINDERS GIRLS COLLEGE REBRANDING</span></h3>
-            </div>
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/work/mathew-flinders.jpg" alt="Mandala">
-        </a>
-    </div><!-- work blocks -->   
-</div><!-- col-md-4 -->
-
-
-
-<div class="row">
-<div class="col-md-4 col-sm-4">
-    <div class="work-blocks">
-        <a href="#" class="overlay-wrapper">
-            <div class="overlay-text">
-                <h3>GVING A BOOK FOR INSTITUTION<br>
-                <span class="work-subtitle">ST KILDA FESTIVAL FUNDRAISER CAMPAIGN</span></h3>
-            </div>
-            <img class="img-hover" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/work/wine-week.jpg" alt="St Kilda Festival">
-        </a>
-    </div><!-- work blocks -->     
-</div><!-- col-md-4 -->
-<div class="col-md-4 col-sm-4">
-    <div class="work-blocks">
-        <a href="#" class="overlay-wrapper">
-            <div class="overlay-text">
-                <h3>STRIPPING BACK THE SUPERFLUOUS<br>
-                <span class="work-subtitle">HOMEGROUND ANNUAL REPORT</span></h3>
-            </div>
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/work/monarch-pharmacy.jpg" alt="Distilled Spirits">
-        </a>
-    </div><!-- work blocks -->   
-</div><!-- col-md-4 -->
-<div class="col-md-4 col-sm-4">
-    <div class="work-blocks">
-        <a href="#" class="overlay-wrapper">
-            <div class="overlay-text">
-                <h3>SHOWING REAL RESULTS<br>
-                <span class="work-subtitle">MATHEW FLINDERS GIRLS COLLEGE REBRANDING</span></h3>
-            </div>
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/work/wlsn-rebrand.jpg" alt="Mandala">
-        </a>
-    </div><!-- work blocks -->   
-</div><!-- col-md-4 -->
 </div><!-- row -->
