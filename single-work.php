92c17aada950b875452485e667e43985cc9a46fd
Josh Richardson <joshrichardson1234@hotmail.com>
2015-12-07 16:25:29 +1100

    Conditional logic added for potentially adding videos on each image

--- single-work.php
+++ single-work.php
@@ -2,12 +2,16 @@
 
 // Advanced Custom Fields
 
-// Grid images
+// Grid images or videos
 $grid_image_1       = get_field('image_1');
 $grid_image_2       = get_field('image_2');
 $grid_image_3       = get_field('image_3');
 $grid_image_4       = get_field('image_4');
 $grid_image_5       = get_field('image_5');
+
+// Video embeds
+$image_1_video      = get_field('image_1_do_you_wish_to_add_a_video');
+$video_embed_code_1 = get_field('video_embed_code_1');
 
 $about              = get_field('about');
 $subtitle           = get_field('subtitle');
@@ -23,13 +27,19 @@ get_template_part( 'template-parts/content', 'carousel' );
     <div class="container">
         <div class="row">
             <div class="col-lg-12 section-title">
-                <h3>United Jewish Education Board</h3>
+                <h3><?php echo get_the_title(); ?></h3>
             </div><!-- title -->
         </div><!-- row -->
         <div class="work-images">
             <div class="row">
                 <div class="col-sm-4">
-                    <img src="<?php echo $grid_image_1['url']; ?>" alt="<?php echo $grid_image_1['alt']; ?>">
+                    <?php if( $image_1_video == "Yes" ): ?>
+                        <a data-toggle="modal" data-target="#myModal" href="#">
+                            <img src="<?php echo $grid_image_1['url']; ?>" alt="<?php echo $grid_image_1['alt']; ?>">
+                        </a>
+                    <?php else: ?>
+                        <img src="<?php echo $grid_image_1['url']; ?>" alt="<?php echo $grid_image_1['alt']; ?>">
+                    <?php endif; ?>
                 </div><!-- col -->
                 <div class="col-sm-4">
                     <img src="<?php echo $grid_image_2['url']; ?>" alt="<?php echo $grid_image_2['alt']; ?>">
@@ -60,6 +70,27 @@ get_template_part( 'template-parts/content', 'carousel' );
         </div><!-- row -->
     </div><!-- container -->
 </section><!-- work -->
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Video</h4>
+      </div>
+      <div class="modal-body">
+        <iframe src=" <?php echo $video_embed_code_1; ?>" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
 
 
 <?php get_template_part( 'template-parts/content', 'otherwork' ); ?>
